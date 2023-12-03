930ad623c2501de1e4a1204b4756e7d6c466dbdc
brunorg21 <rbrunog21@gmail.com>
2023-12-03 00:22:24 -0300

    add cors in server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { fastify } from "fastify";
+import cors from "@fastify/cors";
 import { userRoutes } from "./routes/user-routes";
 import { prisma } from "../prisma/prismaClient";
 import { postRoutes } from "./routes/post-routes";
 import { commentRoutes } from "./routes/comment-routes";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+  methods: ["PUT", "POST", "GET", "DELETE"],
+});
 
 app.register(commentRoutes);
 app.register(userRoutes);
