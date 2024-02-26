afce4a5ada94fbd2984427503db324e8c0e29315
brunorg21 <rbrunog21@gmail.com>
2024-02-26 15:37:44 -0300

    chore: add build script

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import cookie, { FastifyCookieOptions } from "@fastify/cookie";
 import fastifyStatic from "@fastify/static";
 import { join } from "path";
 import { followerRoute } from "./routes/follower-route";
+import { env } from "./env";
 
 const app = fastify();
 
@@ -40,7 +41,7 @@ app.register(followerRoute);
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("Server is running...");
