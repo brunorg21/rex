2d4c51eab0161816f9ce4b56418979c6dad83a84
brunorg21 <rbrunog21@gmail.com>
2024-02-26 22:17:55 -0300

    refactor: change cookie method

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -74,7 +74,6 @@ export class UserController {
       path: "/",
       maxAge: 1 * 60 * 60 * 24, //1 dia
       httpOnly: true,
-      domain: "https://rex-front-pied.vercel.app",
       secure: false,
       sameSite: "none",
     });
